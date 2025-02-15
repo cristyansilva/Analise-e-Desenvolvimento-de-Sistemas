69b304553237b07bfc1a4e025d1ab4a6d622ad5e
cristyansilva <cristyannevessilva@gmail.com>
2025-02-15 15:38:58 -0300

    commit

---
+++ aula-07.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, a multiplicação é {} \na divisão é {}, a Divisão Inteira é {} e a portência é {}' .format(s, m, d, di, e))
