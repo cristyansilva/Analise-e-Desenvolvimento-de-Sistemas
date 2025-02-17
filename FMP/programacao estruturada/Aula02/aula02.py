4331711d6fb0af7aeb83dd8e076bb2ade921aec9
cristyansilva <cristyannevessilva@gmail.com>
2025-02-17 10:07:00 -0300

    commit

---
+++ FMP/programacao estruturada/Aula02/aula02.py
@@ -0,0 +1,13 @@
+# exemplo1
+'''
+print('Olá mundo')
+print('Olá mundo, tudo bem?')
+print('Olá mundo"\nTudo bem?')
+print('Olá, mundo')
+print('Tudo bem?')
+'''
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite a sua altura: '))
+
+print(f'{nome}, minha altura é: {altura} e tenho {idade} anos')
