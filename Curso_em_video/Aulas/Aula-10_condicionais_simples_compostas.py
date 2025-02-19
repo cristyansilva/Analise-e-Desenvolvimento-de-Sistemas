c615fef79a95932ce29f175188e6718e4568e40b
cristyansilva <cristyannevessilva@gmail.com>
2025-02-19 17:56:55 -0300

    commit

--- Curso_em_video/Aulas/Aula-10_condicionais_simples_compostas.py
+++ Curso_em_video/Aulas/Aula-10_condicionais_simples_compostas.py
@@ -1,2 +1,24 @@
-#como utilizar estruturas condicionais simples e compostas
+# como utilizar estruturas condicionais simples e compostas
 
+# if = se    = simples
+# else  = se nao    = composta
+
+'''
+exemplo1: 
+tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <= 3:  # se o carro for menor ou igual a 3 anos
+    print('Carro novo')  # é novo
+else:  # se não
+    print('Carro velho')
+print('--FIM__')
+'''
+# exemplo2
+n1 = float(input('Digite a Primeira nota: '))
+n2 = float(input('Digite a Segunda nota: '))
+n3 = float(input('Digite a Terceira nota: '))
+s = ((n1*2) + (n2*2) + (n3*1))/5
+print(f'Sua média foi: {s}.')
+if s >= 7:
+    print('Parabéns, você foi Aprovado!')
+else:
+    print('Reprovado! Estude mais no próximo semestre!')
